946b47caec8e18ca0f91b1363a989cbc652afba1
WaPhanuwa <124597480+WaPhanuwa@users.noreply.github.com>
2024-06-04 19:34:24 +0700

    Add a page ejs

--- app1.js
+++ app1.js
@@ -3,19 +3,17 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000; // Use process.env.PORT or default to 4000
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Set views directory and view engine
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-    const indexPath = path.join(__dirname, 'public', 'index.html');
-    
-    res.sendFile(indexPath, (err) => {
-        if (err) {
-            res.send('Phabuwat');
-        }
-    });
+    res.render('index', { username: 'phanuwat',customers: ["dawnapa","kamon","supasi"] });
 });
 
 app.listen(port, () => {
