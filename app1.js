c11fcf30e1cd28828e011520880e526e352dfae0
WaPhanuwa <124597480+WaPhanuwa@users.noreply.github.com>
2024-06-04 19:43:58 +0700

    Add a products

--- app1.js
+++ app1.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-
+const productsRouter = express.Router();
 const app = express();
 const port = process.env.PORT || 4000; // Use process.env.PORT or default to 4000
 
@@ -11,6 +11,18 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Set views directory and view engine
 app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'ejs');
+
+productsRouter.route("/").get((req, res)=> {
+    res.send("I am products");
+});
+
+productsRouter.route("/1").get((req, res)=> {
+    res.send("I am products 1");
+});
+
+app.use("/products", productsRouter);
+
+app.get("/products");
 
 app.get("/", (req, res) => {
     res.render('index', { username: 'phanuwat',customers: ["dawnapa","kamon","supasi"] });
